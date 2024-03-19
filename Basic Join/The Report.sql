ec53e78fa1e5dd402c1b35043fc0213131252e8a
Ppsc01 <52832393+PhyoChit@users.noreply.github.com>
2024-03-19 01:55:33 +0100

    Update The Report.sql

--- Basic Join/The Report.sql
+++ Basic Join/The Report.sql
@@ -1,6 +1,7 @@
 SELECT 
 CASE WHEN grd.grade < 8 THEN NULL 
 WHEN grd.grade >= 8 THEN std.name END,
-grd.grade, std.marks FROM students std, grades grd
-WHERE std.marks BETWEEN grd.min_mark AND grd.max_mark
+grd.grade, std.marks 
+FROM students std
+JOIN grades AS grd ON std.marks BETWEEN grd.min_mark AND grd.max_mark
 ORDER BY grd.grade DESC, std.name ASC;
